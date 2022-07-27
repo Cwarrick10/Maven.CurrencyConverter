1aabace7d40f88487732219d0d98cef91fa1c16f
Cwarrick10 <warrickchar@gmail.com>
2022-07-27 11:00:03 -0400

    finishing

--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/Ringgit.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/Ringgit.java
@@ -1,6 +1,12 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
-
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 public class Ringgit implements ConvertableCurrency {
+    CurrencyType ring = CurrencyType.RINGGIT;
+
+    @Override
+    public CurrencyType getType() {
+        return ring;
+    }
 }
--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/UniversalCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/UniversalCurrency.java
@@ -1,6 +1,12 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
-
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 public class UniversalCurrency implements ConvertableCurrency {
+    CurrencyType ucDollar = CurrencyType.UNIVERSAL_CURRENCY;
+
+    @Override
+    public CurrencyType getType() {
+        return ucDollar;
+    }
 }
